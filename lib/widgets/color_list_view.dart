df99e907c3964cb6d12bb4f8f57ca5d4fe34ffff
TechAurelian <dev@techaurelian.com>
2024-09-30 15:13:35 +0300

    Try to fix color list view keyboard navigation

--- lib/widgets/color_list_view.dart
+++ lib/widgets/color_list_view.dart
@@ -22,7 +22,7 @@ typedef ItemButtonData = ({
 });
 
 /// A list view that displays a list of available colors.
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({
     super.key, // ignore: unused_element
     this.scrollController,
@@ -52,23 +52,36 @@ class ColorListView extends StatelessWidget {
   final void Function(int index)? onItemButtonPressed;
 
   @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int focusedIndex = -1;
+
+  @override
   Widget build(BuildContext context) {
     return Scrollbar(
-      controller: scrollController,
+      controller: widget.scrollController,
       thumbVisibility: true,
       child: ListView.builder(
-        controller: scrollController,
-        itemCount: itemCount(),
+        controller: widget.scrollController,
+        itemCount: widget.itemCount(),
         itemExtent: consts.colorListItemExtent,
         itemBuilder: (BuildContext context, int index) {
-          final ColorListItemData item = itemData(index);
+          final ColorListItemData item = widget.itemData(index);
           return _ColorListItem(
             color: item.color,
             title: item.title,
             subtitle: item.subtitle,
-            itemButton: itemButton?.call(index),
-            onTap: () => onItemTap?.call(index),
-            onButtonPressed: () => onItemButtonPressed?.call(index),
+            itemButton: widget.itemButton?.call(index),
+            focused: index == focusedIndex,
+            onTap: () => widget.onItemTap?.call(index),
+            onButtonPressed: () => widget.onItemButtonPressed?.call(index),
+            onFocusChange: (bool hasFocus) {
+              setState(() {
+                focusedIndex = hasFocus ? index : -1;
+              });
+            },
           );
         },
       ),
@@ -76,6 +89,7 @@ class ColorListView extends StatelessWidget {
   }
 }
 
+/// A list item that displays a color with a title and an optional subtitle and button.
 class _ColorListItem extends StatelessWidget {
   const _ColorListItem({
     super.key, // ignore: unused_element
@@ -83,23 +97,33 @@ class _ColorListItem extends StatelessWidget {
     required this.title,
     this.subtitle,
     this.itemButton,
+    this.focused = false,
     this.onTap,
     this.onButtonPressed,
+    this.onFocusChange,
   });
 
+  /// The color of the list item.
   final Color color;
 
+  /// The title of the list item.
   final String title;
 
+  /// The optional subtitle of the list item.
   final String? subtitle;
 
+  /// Data for the optional button of the list item.
   final ItemButtonData? itemButton;
+
+  final bool focused;
 
   /// The function to call when the list item is tapped.
   final void Function()? onTap;
 
   /// A callback function that is called when the button of this list item is pressed.
   final void Function()? onButtonPressed;
+
+  final void Function(bool)? onFocusChange;
 
   @override
   Widget build(BuildContext context) {
@@ -108,9 +132,35 @@ class _ColorListItem extends StatelessWidget {
     final Color contrastColor = color_utils.contrastColor(color);
 
     return InkWell(
+      // focusColor: color.withOpacity(0.75),
+      // focusColor: color,
+      onFocusChange: onFocusChange,
+
+      // borderRadius: BorderRadius.circular(8.0),
+      // focusColor: contrastColor.withOpacity(0.5),
+
       onTap: onTap,
       child: Ink(
-        color: color,
+        // color: color,
+
+        decoration: BoxDecoration(
+          color: color,
+          // borderRadius: BorderRadius.circular(8.0),
+          // border: Border(
+          //   left: BorderSide(
+          //     color: focused ? Colors.grey[700]! : color,
+          //     width: 8.0,
+          //   ),
+          // ),
+          border: Border.all(
+            // color: focused ? Colors.black : color,
+            // color: focused ? Colors.black : color,
+            color: focused ? Colors.grey[700]! : color,
+            // color: focused ? contrastColor : color,
+            width: 8.0,
+          ),
+        ),
+
         // Use padding to constrain the width of the list items so they look ok on large screens
         padding: EdgeInsets.symmetric(
           horizontal: max(16.0, (width - 1024) / 2),
@@ -125,12 +175,19 @@ class _ColorListItem extends StatelessWidget {
               children: <Widget>[
                 Text(
                   title,
-                  style: Theme.of(context).textTheme.titleMedium!.copyWith(color: contrastColor),
+                  style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                        color: contrastColor,
+                        // fontSize: focused ? 22.0 : null,
+                        // fontWeight: focused ? FontWeight.w900 : null,
+                      ),
                 ),
                 if (subtitle != null)
                   Text(
                     subtitle!,
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: contrastColor),
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: contrastColor,
+                          // fontWeight: focused ? FontWeight.w900 : null,
+                        ),
                   ),
               ],
             ),
@@ -139,6 +196,7 @@ class _ColorListItem extends StatelessWidget {
                 icon: Icon(itemButton!.icon, color: color_utils.contrastIconColor(color)),
                 tooltip: itemButton!.tooltip,
                 onPressed: onButtonPressed,
+                focusColor: contrastColor.withOpacity(0.25),
               )
           ],
         ),
